e0ee74e13498916f8edaf3a7ff57ea7918993d61
zfy. <fyzhou@liangzhicn.com>
2026-04-08 14:07:38 +0800

    update

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -179,7 +179,7 @@ export default ({
   return (
     <div class="md:py-2 md:px-4 transition-colors group">
       <div class={`flex rounded-lg ${isUserMessage ? 'justify-end' : ''}`}>
-        <div ref={messageRef!} class={`message prose break-words overflow-hidden ${isUserMessage ? `max-w-[85%] bg-slate/8 dark:bg-slate/15 rounded-2xl px-4 py-2${isEditing() ? ' w-[85%]' : ''}` : 'flex-1'}`}>
+        <div ref={messageRef!} class={`message prose break-words overflow-hidden ${isUserMessage ? `max-w-[85%] bg-slate/8 dark:bg-slate/15 rounded-2xl px-4 ${isEditing() ? ' w-[85%]' : ''}` : 'flex-1'}`}>
           <Show when={isEditing()}>
               <textarea
                 ref={(el) => {
